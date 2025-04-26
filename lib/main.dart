f841cd239672f46849260755f0aac7032de777b6
hussainxgit <robinx5.q8@gmail.com>
2025-04-26 23:37:20 +0300

    Refactor AddDoctorForm to use ConsumerWidget and integrate form controllers; update main.dart to remove unused Firebase service initialization

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'core/config/app_theme.dart';
 import 'core/config/dependencies.dart';
-import 'core/services/firebase/firebase_service.dart';
 import 'features/dashboard/view/dashboard_screen.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   // Load environment variables
   await dotenv.load();
 
   // Initialize Firebase services
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
-  // Initialize other services
-  final firebaseService = FirebaseService();
-  await setupDependencies(firebaseService);
-
   
   runApp(const ProviderScope(child: MyApp()));
 }
